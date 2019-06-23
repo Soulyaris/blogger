14aa08c725b892bdab9d2876149d3853cf599bb6
Soulyaris <genessa174@gmail.com>
2019-06-23 12:41:10 +0500

    added a few gems

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,5 +7,12 @@ class TagsController < ApplicationController
   def index
     @tag = Tag.all
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect_to tags_path   
+  end
   
 end
